29689b05e2570fa281af7ac994dc97ce69debf89
unknown <nikitasaxena.net@gmail.com>
2026-05-07 11:00:08 +0530

    Initial commit

---
+++ practice/src/components/CourseList.jsx
@@ -0,0 +1,59 @@
+import React from 'react'
+import useCourseStore from '../app/courseStore'
+
+const CourseList = () => {
+
+    const courses = useCourseStore(
+        (state) => state.courses
+    )
+
+    const removeCourse = useCourseStore(
+        (state) => state.removeCourse
+    )
+
+    const toggleCourseStatus = useCourseStore(
+        (state) => state.toggleCourseStatus
+    )
+
+  return (
+    <ul>
+        {courses.map((course) => {
+            return (
+                <li
+                    key={course.id}
+                    className="course-item"
+                    style={{
+                        backgroundColor:
+                          course.completed
+                          ? "#00FF0044"
+                          : "white"
+                    }}
+                >
+                    <span className="course-item-col-1">
+                        <input
+                            checked={course.completed}
+                            type="checkbox"
+                            onChange={() => {
+                                toggleCourseStatus(course.id)
+                            }}
+                        />
+                    </span>
+
+                    <span>{course.title}</span>
+
+                    <button
+                        onClick={() => {
+                            removeCourse(course.id)
+                        }}
+                        className="delete-btn"
+                    >
+                        Delete
+                    </button>
+                </li>
+            )
+        })}
+    </ul>
+  )
+}
+
+export default CourseList
